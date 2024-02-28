e84da928d55f97f76e3f2ff003adc9f0897a1fa5
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-28 08:18:48 +0800

    feat: create page

--- src/pages/tv-series/tv-series.page.tsx
+++ src/pages/tv-series/tv-series.page.tsx
@@ -1,5 +1,12 @@
+import MediaView from '../../components/media-view/media-view.component';
+import ViewsContainer from '../../styles/common/views-container';
+
 const TVSeries = () => {
-  return <div></div>;
+  return (
+    <ViewsContainer>
+      <MediaView title='TV Series' urlEndpoint='/fetchTVSeries' />
+    </ViewsContainer>
+  );
 };
 
 export default TVSeries;

62748ee2b36f7b8133cb175987663afb52a9fa39
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-28 08:18:26 +0800

    feat: create page

--- src/pages/movies/movies.page.tsx
+++ src/pages/movies/movies.page.tsx
@@ -1,5 +1,12 @@
+import MediaView from '../../components/media-view/media-view.component';
+import ViewsContainer from '../../styles/common/views-container';
+
 const Movies = () => {
-  return <div>This is the movie page</div>;
+  return (
+    <ViewsContainer>
+      <MediaView title='Movies' urlEndpoint='/fetchMovies' />
+    </ViewsContainer>
+  );
 };
 
 export default Movies;

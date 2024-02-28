cd25810428d5f340976d7feaa86858cd3e470668
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-28 08:18:08 +0800

    feat: create page

--- src/pages/main/main.page.tsx
+++ src/pages/main/main.page.tsx
@@ -1,13 +1,24 @@
 import { Outlet } from 'react-router-dom';
 
 import NavBar from '../../components/nav-bar/nav-bar.component';
+import SearchBar from '../../components/search-bar/search-bar.component';
+import {
+  StyledContainer,
+  StyledPageContainer,
+  StyledSection
+} from './main.styles';
 
 const Main = () => {
   return (
-    <>
+    <StyledContainer>
       <NavBar />
-      <Outlet />
-    </>
+      <StyledSection>
+        <StyledPageContainer>
+          <SearchBar />
+          <Outlet />
+        </StyledPageContainer>
+      </StyledSection>
+    </StyledContainer>
   );
 };
 

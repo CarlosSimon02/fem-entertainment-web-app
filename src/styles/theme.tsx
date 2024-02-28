7cd7b729f8139487e9ef72b8fa406ac4b1a3ef14
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-28 08:22:08 +0800

    style: created theme

---
+++ src/styles/theme.tsx
@@ -0,0 +1,29 @@
+const theme = {
+  colors: {
+    primary: {
+      main: 'hsl(222, 30.434782608695656%, 9.019607843137255%)',
+      sub: 'hsl(223, 36.231884057971016%, 13.529411764705884%)',
+      button: 'hsl(222, 30.434782608695656%, 9.019607843137255%, 50%)'
+    },
+    secondary: {
+      main: 'hsl(0, 0%, 100%)',
+      sub: 'hsl(223, 22.746781115879823%, 45.68627450980392%)'
+    },
+    accent: {
+      main: 'hsl(0, 96.79144385026738%, 63.33333333333333%)'
+    }
+  },
+  fontWeights: {
+    light: 300,
+    medium: 500
+  },
+  body: {
+    margin: {
+      mobile: 16,
+      tablet: 24,
+      desktop: 32
+    }
+  }
+};
+
+export default theme;

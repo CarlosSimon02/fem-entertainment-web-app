e830a41a7668eae7392f5a96098c237efa644e7e
CarlosSimon02 <caloycamacho02@gmail.com>
2024-02-28 08:17:30 +0800

    feat: create page

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,52 +1,18 @@
-import { useEffect, useState } from 'react';
-
-const getTrending = async () => {
-  const url = '/.netlify/functions/getTrending';
-
-  try {
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        accept: 'application/json'
-      }
-    });
-    console.log(response);
-    if (!response.ok) {
-      console.error(
-        'Error fetching data:',
-        response.status,
-        response.statusText
-      );
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data.movies;
-  } catch (error: any) {
-    console.error('Error handling response:', error.message);
-  }
-};
+import MediaView, {
+  MediaViewType
+} from '../../components/media-view/media-view.component';
+import ViewsContainer from '../../styles/common/views-container';
 
 const Home = () => {
-  const [trending, setTrending] = useState<string[]>([]);
-
-  useEffect(() => {
-    getTrending()
-      .then((movies) => {
-        console.log(movies);
-        setTrending(movies);
-      })
-      .catch((error) => {
-        console.error('Error:', error.message);
-      });
-  }, []);
-
   return (
-    <div>
-      {trending.map((movie: string, index) => (
-        <h1 key={index}>{movie}</h1>
-      ))}
-    </div>
+    <ViewsContainer>
+      <MediaView
+        title='Trending'
+        urlEndpoint='/fetchTrending'
+        type={MediaViewType.Horizontal}
+      />
+      <MediaView title='Recommended For You' urlEndpoint='/fetchRecommended' />
+    </ViewsContainer>
   );
 };
 

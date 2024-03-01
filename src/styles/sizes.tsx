9624ea7fcd11d7e21905ef11dc3b6ee0d7aa2456
CarlosSimon02 <caloycamacho02@gmail.com>
2024-03-01 22:40:53 +0800

    feat: add sizes

--- src/styles/sizes.tsx
+++ src/styles/sizes.tsx
@@ -1,34 +1,37 @@
 export const fontWeights = {
   light: 300,
-  medium: 500
+  regular: 400,
+  medium: 500,
+  semiBold: 600,
+  bold: 700
 };
 
 export const breakpoints = {
   up: {
-    xs: 375, // for small screen mobile
-    sm: 768, // for tablets
-    md: 1000, // for laptops
-    lg: 1450 // for desktop / monitors
+    mobile: 390, // for small screen mobile
+    tablet: 830, // for tablets
+    laptop: 1440, // for laptops
+    desktop: 1920 // for desktop / monitors
   },
   down: {
-    xs: 374.99, // for small screen mobile
-    sm: 767.99, // for tablets
-    md: 999.99, // for laptops
-    lg: 1449.99 // for desktop / monitors
+    mobile: 389.99, // for small screen mobile
+    tablet: 829.99, // for tablets
+    laptop: 1439.99, // for laptops
+    desktop: 1919.99 // for desktop / monitors
   }
 };
 
 export const device = {
   up: {
-    xs: `(min-width: ${breakpoints.down.xs / 16}em)`,
-    sm: `(min-width: ${breakpoints.down.sm / 16}em)`,
-    md: `(min-width: ${breakpoints.down.md / 16}em)`,
-    lg: `(min-width: ${breakpoints.down.lg / 16}em)`
+    mobile: `(min-width: ${breakpoints.down.mobile / 16}em)`,
+    tablet: `(min-width: ${breakpoints.down.tablet / 16}em)`,
+    laptop: `(min-width: ${breakpoints.down.laptop / 16}em)`,
+    desktop: `(min-width: ${breakpoints.down.desktop / 16}em)`
   },
   down: {
-    xs: `(max-width: ${breakpoints.up.xs / 16}em)`,
-    sm: `(max-width: ${breakpoints.up.sm / 16}em)`,
-    md: `(max-width: ${breakpoints.up.md / 16}em)`,
-    lg: `(max-width: ${breakpoints.up.lg / 16}em)`
+    mobile: `(max-width: ${breakpoints.up.mobile / 16}em)`,
+    tablet: `(max-width: ${breakpoints.up.tablet / 16}em)`,
+    laptop: `(max-width: ${breakpoints.up.laptop / 16}em)`,
+    desktop: `(max-width: ${breakpoints.up.desktop / 16}em)`
   }
 };

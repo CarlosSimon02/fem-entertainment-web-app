82777f061e8fed13111c10da1490127ba9c587a8
CarlosSimon02 <caloycamacho02@gmail.com>
2024-03-01 22:39:19 +0800

    feat(route): add route

---
+++ src/routes/main/main.styles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledBodySection = styled.section`
+  flex-grow: 1;
+`;
+
+export const StyledWrapper = styled.div`
+  align-items: stretch;
+  display: flex;
+  flex-direction: column;
+  justify-content: stretch;
+  min-height: 100vh;
+  width: 100vw;
+`;

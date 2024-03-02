bae33f59a4b4e3601426e6ef863e07d106f357c2
CarlosSimon02 <caloycamacho02@gmail.com>
2024-03-02 22:35:58 +0800

    feat(route): add route

---
+++ src/routes/home/home.route.tsx
@@ -0,0 +1,13 @@
+import LatestMediaView from '@/components/latest-media-view/latest-media-view.component';
+
+import { StyledSection } from './home.styles';
+
+const Home = () => {
+  return (
+    <StyledSection>
+      <LatestMediaView />
+    </StyledSection>
+  );
+};
+
+export default Home;

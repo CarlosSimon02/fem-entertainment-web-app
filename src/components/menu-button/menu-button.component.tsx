861c0df8582d351f67474fe4d43070d2270a9145
CarlosSimon02 <caloycamacho02@gmail.com>
2024-03-03 22:29:50 +0800

    feat(component): add component

---
+++ src/components/menu-button/menu-button.component.tsx
@@ -0,0 +1,11 @@
+import { StyledButton, StyledHamburgerSVG } from './menu-button.styles';
+
+const MenuButton = () => {
+  return (
+    <StyledButton>
+      <StyledHamburgerSVG />
+    </StyledButton>
+  );
+};
+
+export default MenuButton;

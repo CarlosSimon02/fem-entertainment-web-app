0e360bdaacf2ff50db5e7bd30b300850bd7c93b8
CarlosSimon02 <caloycamacho02@gmail.com>
2024-03-05 22:14:25 +0800

    chore: remove file

--- src/pages/router-error/router-error.page.tsx
+++
@@ -1,8 +0,0 @@
-import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
-
-const RouterError = () => {
-  const error = useRouteError() as Error;
-  return <div>{isRouteErrorResponse(error) && error.statusText}</div>;
-};
-
-export default RouterError;

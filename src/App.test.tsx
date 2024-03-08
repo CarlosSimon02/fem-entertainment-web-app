636224516d86fd37bd321cc12f8ba1da0a74a186
CarlosSimon02 <caloycamacho02@gmail.com>
2024-03-08 22:01:50 +0800

    chore: update file paths for consistency

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-
 import { render, screen } from '@testing-library/react';
 
-import App from './app.main';
+import App from './app';
 
 test('renders learn react link', () => {
   render(<App />);

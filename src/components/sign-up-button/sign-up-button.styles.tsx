587cb646534943d5d544c132109df4a25de8807e
CarlosSimon02 <caloycamacho02@gmail.com>
2024-03-11 18:04:24 +0800

    style: update styles for sign-up-button

--- src/components/sign-up-button/sign-up-button.styles.tsx
+++ src/components/sign-up-button/sign-up-button.styles.tsx
@@ -1,16 +1,6 @@
-import { fontWeights } from '@/styles/sizes';
-import { fluidSizeLaptopToDesktop, pxToRem } from '@/utils/functions';
+import { AccentButtonStyle } from '@/styles/mixins';
 import styled from 'styled-components';
 
 export const StyledAccentButton = styled.button`
-  background-color: ${({ theme }) => theme.color.accentButtonBg};
-  border-radius: ${pxToRem(8)};
-  border-style: none;
-  color: ${({ theme }) => theme.color.buttonFg};
-  cursor: pointer;
-  flex-shrink: 0;
-  font-size: ${fluidSizeLaptopToDesktop(14, 18)};
-  font-weight: ${fontWeights.semiBold};
-  padding-block: ${pxToRem(14)};
-  padding-inline: ${pxToRem(24)};
+  ${AccentButtonStyle}
 `;

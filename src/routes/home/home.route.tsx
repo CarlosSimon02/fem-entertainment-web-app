49eac20718dc0afbda146a42186a491ef37bb02c
CarlosSimon02 <caloycamacho02@gmail.com>
2024-03-11 18:06:36 +0800

    feat(route): update route

--- src/routes/home/home.route.tsx
+++ src/routes/home/home.route.tsx
@@ -1,10 +1,37 @@
-import LatestMediaView from '@/components/latest-media-view/latest-media-view.component';
+import CommonCarousel from '@/components/common-carousel/common-carousel.component';
+import UpcomingMoviesSlideshow from '@/components/upcoming-movies-slideshow/upcoming-movies-slideshow.component';
 
 const Home = () => {
   return (
-    <div>
-      <LatestMediaView />
-    </div>
+    <>
+      <UpcomingMoviesSlideshow />
+      <CommonCarousel
+        title='Now Showing'
+        url='/.netlify/functions/fetchNowShowing?type=all'
+      />
+      <CommonCarousel
+        title='Popular'
+        url='/.netlify/functions/fetchPopular?type=all'
+      />
+      <CommonCarousel
+        title='Trending'
+        url='/.netlify/functions/fetchTrending?type=all'
+      />
+      <CommonCarousel
+        title='Top Rated'
+        url='/.netlify/functions/fetchTopRated?type=all'
+      />
+      <CommonCarousel
+        title='Movie Genres'
+        url='/.netlify/functions/fetchGenres?type=movies'
+        type='genre'
+      />
+      <CommonCarousel
+        title='TV Series Genres'
+        url='/.netlify/functions/fetchGenres?type=tvseries'
+        type='genre'
+      />
+    </>
   );
 };
 

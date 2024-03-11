364e211c6f9a5d239d404d66657ca8b5d71e5b5a
CarlosSimon02 <caloycamacho02@gmail.com>
2024-03-11 18:08:29 +0800

    chore: add AccentButtonStyle mixin

--- src/styles/mixins.tsx
+++ src/styles/mixins.tsx
@@ -1,7 +1,11 @@
-import { fluidSizeAllDevice, pxToRem } from '@/utils/functions';
+import {
+  fluidSizeAllDevice,
+  fluidSizeLaptopToDesktop,
+  pxToRem
+} from '@/utils/functions';
 import { css } from 'styled-components';
 
-import { breakpoints } from './sizes';
+import { breakpoints, fontWeights } from './sizes';
 
 export const SpacerSectionStyle = css`
   max-width: ${pxToRem(breakpoints.up.desktop)};
@@ -9,4 +13,21 @@ export const SpacerSectionStyle = css`
   width: 100%;
 
   ${fluidSizeAllDevice('padding-inline', [16, 32, 80, 162])}
+`;
+
+export const AccentButtonStyle = css`
+  align-items: center;
+  background-color: ${({ theme }) => theme.color.accentButtonBg};
+  border-radius: ${pxToRem(8)};
+  border-style: none;
+  color: ${({ theme }) => theme.color.buttonFg};
+  cursor: pointer;
+  display: flex;
+  flex-shrink: 0;
+  font-size: ${fluidSizeLaptopToDesktop(14, 18)};
+  font-weight: ${fontWeights.semiBold};
+  gap: ${pxToRem(9)};
+  justify-content: center;
+  padding-block: ${fluidSizeLaptopToDesktop(12, 14)};
+  padding-inline: ${pxToRem(24)};
 `;

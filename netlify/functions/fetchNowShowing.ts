1d52a151684cd5db773cb35b929f08bcfcbf20e0
CarlosSimon02 <caloycamacho02@gmail.com>
2024-03-13 17:44:03 +0800

    chore(serverless): add function to fetch now showing

---
+++ netlify/functions/fetchNowShowing.ts
@@ -0,0 +1,59 @@
+import { Handler } from '@netlify/functions';
+
+import {
+  createCustomResponse,
+  fetchTMDBData,
+  mapToMediaListItemInfo,
+  sortByVoteCount
+} from '../utils/helper-functions';
+import {
+  MediaCarouselSlideInfo,
+  ResponseMediaCarouselSlideInfo,
+  StatusCode
+} from '../utils/types.d';
+
+const handler: Handler = async (event, context) => {
+  if (!event.queryStringParameters)
+    return createCustomResponse(StatusCode.Error, {
+      error:
+        'Please include type as queryStringParameteres (e.g. "all", "movies","tvseries")'
+    });
+
+  const { type } = event.queryStringParameters;
+
+  try {
+    let results: ResponseMediaCarouselSlideInfo[] = [];
+
+    if (type === 'all' || type === 'movies') {
+      const { results: tvSeriesResults } = await fetchTMDBData(
+        '/tv/airing_today?language=en-US&page=3'
+      );
+      results = [...results, ...tvSeriesResults];
+    }
+
+    if (type === 'all' || type === 'tvseries') {
+      const { results: moviesResults } = await fetchTMDBData(
+        '/movie/now_playing?language=en-US&page=3'
+      );
+
+      results = [...results, ...moviesResults];
+    }
+
+    let listItems = results.map(
+      (mediaListItem: ResponseMediaCarouselSlideInfo): MediaCarouselSlideInfo =>
+        mapToMediaListItemInfo(mediaListItem)
+    );
+
+    listItems = sortByVoteCount(listItems).slice(0, 20);
+
+    return createCustomResponse(StatusCode.Success, {
+      listItems
+    });
+  } catch {
+    return createCustomResponse(StatusCode.Error, {
+      error: 'Netlify serverless function error'
+    });
+  }
+};
+
+export { handler };

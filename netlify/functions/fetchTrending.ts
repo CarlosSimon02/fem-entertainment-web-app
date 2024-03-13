45169eb4261377855888561119243006eb5c1178
CarlosSimon02 <caloycamacho02@gmail.com>
2024-03-13 17:45:37 +0800

    feat(serverless): add function to fetch trending

--- netlify/functions/fetchTrending.ts
+++ netlify/functions/fetchTrending.ts
@@ -3,20 +3,56 @@ import { Handler } from '@netlify/functions';
 import {
   createCustomResponse,
   fetchTMDBData,
-  mapToMediaListItemInfo
+  mapToMediaListItemInfo,
+  sortByVoteCount
 } from '../utils/helper-functions';
-import { ResponseMediaListItemInfo, StatusCode } from '../utils/types.d';
+import {
+  MediaCarouselSlideInfo,
+  ResponseMediaCarouselSlideInfo,
+  StatusCode
+} from '../utils/types.d';
 
-const handler: Handler = async () => {
-  try {
-    const { results } = await fetchTMDBData('/trending/all/day?language=en-US');
-    const mediaListItems = results.map((item: ResponseMediaListItemInfo) =>
-      mapToMediaListItemInfo(item)
-    );
-    return createCustomResponse(StatusCode.Success, {
-      mediaListItems
+const handler: Handler = async (event, context) => {
+  if (!event.queryStringParameters)
+    return createCustomResponse(StatusCode.Error, {
+      error:
+        'Please include type as queryStringParameteres (e.g. "all", "movies","tvseries")'
     });
-  } catch (unknownError) {
+
+  const { type } = event.queryStringParameters;
+
+  try {
+    let url: string;
+
+    switch (type) {
+      case 'all':
+        url = '/trending/all/day?language=en-US';
+        break;
+      case 'movies':
+        url = '/trending/movie/day?language=en-US';
+        break;
+      case 'tvseries':
+        url = '/trending/tv/day?language=en-US';
+        break;
+      default:
+        return createCustomResponse(StatusCode.Error, {
+          error: 'queryStringParameteres should only be movies or tvseries'
+        });
+    }
+
+    const { results } = await fetchTMDBData(url);
+
+    let listItems = results.map(
+      (mediaListItem: ResponseMediaCarouselSlideInfo): MediaCarouselSlideInfo =>
+        mapToMediaListItemInfo(mediaListItem)
+    );
+
+    listItems = sortByVoteCount(listItems).slice(0, 20);
+
+    return createCustomResponse(StatusCode.Success, {
+      listItems
+    });
+  } catch {
     return createCustomResponse(StatusCode.Error, {
       error: 'Netlify serverless function error'
     });
